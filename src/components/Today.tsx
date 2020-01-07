3d2480244107ba22deb92c4bba1ded6de0fe57de
cutelee <soyoung.dev@gmail.com>
2020-01-07 12:56:13 +0900

    요일 정보 오늘 날짜로 수정

--- src/components/Today.tsx
+++ src/components/Today.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { Text } from 'react-native';
 
+const week: string[] = ['일', '월', '화', '수', '목', '금', '토'];
+
 export default function Today() {
+  const today = new Date();
+
+  const year = today.getFullYear();
+  const month = today.getMonth() + 1;
+  const date = today.getDate();
+  const day = week[today.getDay()];
+
   return (
-    <Text>오늘은 12월 1일 월요일입니다.</Text>
+    <Text>오늘은 {year}년 {month}월 {date}일 {day}요일입니다.</Text>
   );
 };
