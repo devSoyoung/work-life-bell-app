330ddfbe35c02ff99c746ac5b1ab874865c58c29
cutelee <soyoung.dev@gmail.com>
2020-01-07 13:39:35 +0900

    스타일 수정

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import { Text, StyleSheet } from 'react-native';
-import { Container } from 'native-base';
+import { Text, StyleSheet, View } from 'react-native';
 
 // state : 출근전, 출근, 퇴근
 export default function Timer() {
   return (
-    <Container>
+    <View style={styles.container}>
       <Text style={styles.subtitle}>오늘 일한 시간</Text>
       <Text style={styles.timer}>00 : 00 : 00</Text>
-    </Container>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    padding: 60,
+    shadowColor: '#000',
+    shadowOffset: { width: 10, height: 5 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+  },
   subtitle: {
     fontSize: 25,
     textAlign: 'center',
--- src/layouts/AppHeader.tsx
+++ src/layouts/AppHeader.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Container, Header, Body, Title } from 'native-base';
+import { StyleSheet, View, Text } from 'react-native';
 import { getStatusBarHeight } from 'react-native-status-bar-height';
 
 export default function AppHeader() {
   return (
-    <Container style={styles.header}>
-      <Header>
-        <Body>
-          <Title>OFF THE WORK : 출근 기록하기</Title>
-        </Body>
-      </Header>
-    </Container>
+    <View style={styles.header}>
+      <Text style={styles.title}>work life bell</Text>
+      <Text style={styles.subTitle}>출퇴근 기록하기</Text>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   header: {
     paddingTop: getStatusBarHeight(),
-    height: 54 + getStatusBarHeight(),
+    // height: 54 + getStatusBarHeight(),
   },
+  title: {
+    textTransform: 'uppercase',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 30,
+    paddingTop: 30,
+  },
+  subTitle: {
+    textAlign: 'center',
+    fontSize: 15,
+    marginBottom: 30,
+  }
 });
 
